b9e8b0b45aecec05f7ef0816aa9bf237b9a735c2
Handre Bezuidenhout <handreb20@gmail.com>
2026-07-14 18:42:08 +0200

    added data.py code

--- cifar10-cnn/data.py
+++ cifar10-cnn/data.py
@@ -28,43 +28,33 @@ CIFAR10_STD = (0.2470, 0.2435, 0.2616)
 
 
 def get_transforms():
-    """
-    Return two separate transform pipelines: one for training (with
-    augmentation), one for test/eval (no augmentation, just ToTensor +
-    Normalize).
 
-    TODO: build train_transform using transforms.Compose([...])
-        - RandomCrop(32, padding=4)  -- common CIFAR-10 augmentation choice
-        - RandomHorizontalFlip()
-        - ToTensor()
-        - Normalize(CIFAR10_MEAN, CIFAR10_STD)
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
+    ])
 
-    TODO: build test_transform using transforms.Compose([...])
-        - ToTensor()
-        - Normalize(CIFAR10_MEAN, CIFAR10_STD)
-        - (no augmentation here — think about why, from today's discussion)
-    """
-    train_transform = None  # TODO
-    test_transform = None  # TODO
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(CIFAR10_MEAN, CIFAR10_STD),
+    ])
+
+    
 
     return train_transform, test_transform
 
 
 def get_dataloaders(batch_size=64, data_dir="./data"):
-    """
-    TODO: load CIFAR10 train and test sets via torchvision.datasets.CIFAR10
-        - root=data_dir, train=True/False, download=True, transform=...
-    TODO: wrap each in a DataLoader
-        - train loader should shuffle=True
-        - test loader should shuffle=False (no need to shuffle eval data)
-    """
+   
     train_transform, test_transform = get_transforms()
 
-    train_set = None  # TODO
-    test_set = None  # TODO
+    train_set = torchvision.datasets.CIFAR10(root=data_dir, train=True, download=True, transform=train_transform)
+    test_set = torchvision.datasets.CIFAR10(root=data_dir, train=False, download=True, transform=test_transform)
 
-    train_loader = None  # TODO
-    test_loader = None  # TODO
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
     return train_loader, test_loader
 
